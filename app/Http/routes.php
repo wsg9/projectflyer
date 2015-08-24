d437c1b5c7c69a5112b0bd421cca6b9e488b4abf
William Gilley <wsg9@txstate.edu>
2015-08-24 01:04:10 -0500

    Remove search page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,5 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
-Route::get('pages/search', 'SearchController@index');
-
 Route::resource('flyers', 'FlyersController');
 Route::get('{zip}/{street}', 'FlyersController@show');
